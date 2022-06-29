f924adb7009682a571032fd935391747376d5050
danieltdang <swe.daniel.dang@gmail.com>
2022-06-29 17:53:06 -0400

    updated jquery file name and added typewriterjs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-// Scrolling effect for navbar
+// Scrolling effect for navbar: https://jquery.com/download/
 jQuery(document).ready(function () {
     jQuery('.button').click(function() {
       targetName = jQuery(this).attr('href');
@@ -7,3 +7,21 @@ jQuery(document).ready(function () {
       return false;
     });
   });
+
+// Typewriter for sub-title: https://github.com/tameemsafi/typewriterjs
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('A simple yet powerful native javascript')
+  .pauseFor(300)
+  .deleteChars(10)
+  .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
+  .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+  .pauseFor(1000)
+  .start();
