7c1475c5ffa8049e9f2153734beb4998b7c0bb56
danieltdang <dtd21b@fsu.edu>
2024-12-18 12:56:04 -0500

    Big update with experiences and new information

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -74,7 +74,7 @@ var myTitleMessages = [
   "hey there!",
   "glad to see you here.",
   "my name is daniel.",
-  "i am a junior at FSU.",
+  "i am a FSU graduate.",
   "i am studying computer science.",
   "i am interested in SWE.",
   "i am a big fan of pho.",
